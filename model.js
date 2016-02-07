266bff3d4492b24dea09defabbd95b39c8610d9b
pitou35 <pierreaugustin.totaro03@gmail.com>
2016-02-07 19:24:35 +0100

    question 6

--- model.js
+++ model.js
@@ -1,3 +1,29 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function Drawing(){
+	this.forms=new Array();
+}
+
+function Rectangle(X, Y, largeur, hauteur, epaisseur, couleur){
+	Form.call(this, couleur, epaisseur);
+	this.X=X;
+	this.Y=Y;
+	this.largeur=largeur;
+	this.hauteur=hauteur;
+}
+Rectangle.prototype=new Form();
+
+function Form(epaisseur, couleur){
+	this.couleur=couleur;
+	this.epaisseur=epaisseur;
+}
+
+function Line(xA, yA, xB, yB, epaisseur, couleur){
+	Form.call(this, couleur, epaisseur);
+	this.xA=xA;
+	this.xB=xB;
+	this.yA=yA;
+	this.yB=yB;
+}
+Rectangle.prototype=new Form();
--- view.js
+++ view.js
@@ -1,2 +1,24 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Rectangle.prototype.paint=function(ctx){
+	ctx.rect(this.X, this.Y, this.X+this.largeur, this.Y+this.hauteur);
+	ctx.stroke();
+}
+
+Line.prototype.paint = function(ctx) {
+	ctx.beginPath();
+    ctx.moveTo(this.xA, this.yA);
+    ctx.lineTo(this.xB, this.yB);
+    ctx.stroke();
+	
+}
+
+Drawing.prototype.paint = function(ctx) {
+     console.log(this.getForms());
+     ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+     ctx.fillRect(0, 0, canvas.width, canvas.height);
+     this.getForms().forEach(function(eltDuTableau) {
+         // now fill the canvas
+         eltDuTableau.paint(ctx);
+     });
+ };
